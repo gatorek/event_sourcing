d9e883b3081b4f7c7868d11d4270b73af0a3967c
Michał Sacewicz <michal.sacewicz@fresha.com>
2022-05-11 22:20:43 +0200

    add guards to command handler

--- lib/event_sourcing/command_handlers/cancel_appointment_handler.ex
+++ lib/event_sourcing/command_handlers/cancel_appointment_handler.ex
@@ -4,9 +4,18 @@ defmodule EventSourcing.CommandHandlers.CancelAppointmentHandler do
 
   @behaviour Commanded.Commands.Handler
 
-  def handle(%Appointment{} = aggregate, %CancelAppointment{} = command) do
-    %CancelAppointment{appointment_id: appointment_id} = command
+  def handle(%Appointment{status: "canceled"}, %CancelAppointment{}) do
+    {:error, "appointment already canceled"}
+  end
 
+  def handle(%Appointment{status: "completed"}, %CancelAppointment{}) do
+    {:error, "cannot cancel completed appointment"}
+  end
+
+  def handle(
+    %Appointment{} = aggregate,
+    %CancelAppointment{appointment_id: appointment_id})
+  do
     Appointment.cancel_appointment(aggregate, appointment_id)
   end
 end
